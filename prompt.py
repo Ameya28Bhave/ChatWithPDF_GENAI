8e2c156aad7fc9890970cf18f74daefdc23cef72
Ameya28Bhave <bhaveameya28@gmail.com>
2024-05-26 13:30:45 -0400

    Web App to chat with PDF
    Implemented a Web App to chat with API

---
+++ prompt.py
@@ -0,0 +1,34 @@
+from langchain.vectorstores.chroma import Chroma
+from dotenv import load_dotenv
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from redundant_filter_retriever import RedundantFilterRetriever
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+embeddings = OpenAIEmbeddings()
+
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings
+)
+
+retriever = RedundantFilterRetriever(
+   embeddings = embeddings,
+   chroma= db 
+)
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever = retriever,
+    chain_type="stuff"
+)
+
+result = chain.run("What is an interesting fact about the English language ?")
+
+print(result)
+
+
---
+++ tchat_main.py
@@ -0,0 +1,36 @@
+from langchain.prompts import HumanMessagePromptTemplate , ChatPromptTemplate, MessagesPlaceholder
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+memory = ConversationSummaryMemory(
+    # chat_memory = FileChatMessageHistory("messages.json"),
+    memory_key="messages" , 
+    return_messages=True,
+    llm = chat
+    )
+
+prompt = ChatPromptTemplate(
+    input_variables = ["content"],
+    messages=[
+        MessagesPlaceholder(variable_name = "messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm = chat,
+    prompt = prompt,
+    memory=memory
+)
+
+while True:
+    content = input(">> ")
+    
+    result = chain({"content" : content})
+    print(result["text"])
